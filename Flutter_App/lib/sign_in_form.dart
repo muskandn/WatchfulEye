517529157ac6e9b9a906bf358a81cd98e5bad9c4
Muskan <muskandn02@gmail.com>
2023-07-02 23:47:13 +0530

    submit function added

--- Flutter_App/lib/sign_in_form.dart
+++ Flutter_App/lib/sign_in_form.dart
@@ -33,4 +33,45 @@ class _SignInFormState extends State<SignInForm> {
       },
     );
   }
+
+  Future<void> _submit() async {
+    if (!_formKey.currentState.validate()) {
+      // Invalid!
+      return;
+    }
+    _formKey.currentState.save();
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      bool isVerified = await Provider.of<Auth>(context, listen: false)
+          .loginWithEmail(_authData['email'], _authData['password']);
+
+      if (isVerified) {
+        Navigator.of(context).pop();
+      } else {
+        _showErrorDialog("Email not verified",
+            "We have just sent you a verification email. Please verify your email before continuing");
+      }
+    } on HttpException catch (error) {
+      var errorMessage = 'Authentication error';
+      if (error.message.contains('ERROR_INVALID_EMAIL')) {
+        errorMessage = 'This email address is invalid';
+      } else if (error.message.contains('ERROR_USER_NOT_FOUND')) {
+        errorMessage = 'Could not find a user with that email address';
+      } else if (error.message.contains('ERROR_WRONG_PASSWORD')) {
+        errorMessage = 'This password is invalid';
+      } else if (error.message.contains('ERROR_TOO_MANY_REQUESTS')) {
+        errorMessage = 'Please try again later';
+      }
+      _showErrorDialog("Something went wrong", errorMessage);
+    } catch (error) {
+      const errorMessage = 'Could not sign you in, please try again later.';
+      _showErrorDialog("Something went wrong", errorMessage);
+      throw error;
+    }
+    setState(() {
+      _isLoading = false;
+    });
+  }
 }
