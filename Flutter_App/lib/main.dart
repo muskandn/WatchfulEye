5a0aa2d273707283d469242a2503d5a37c787eaa
Muskan <muskandn02@gmail.com>
2023-07-04 19:38:00 +0530

    Main file has been updated

--- Flutter_App/lib/main.dart
+++ Flutter_App/lib/main.dart
@@ -1,41 +1,47 @@
 import 'dart:ui';
 
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/services.dart';
+import 'package:geocoder/geocoder.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:video_player/video_player.dart';
+
+import 'login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'auth.dart';
+
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
+
+void _showErrorDialog(BuildContext context, String title, String message) {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text(title),
+        content: Text(message),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text('OK'),
+          )
+        ],
+      );
+    },
+  );
+}
+
+bool exit = false;
 
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-          return MaterialApp(
-            title: 'HawkEye',
-            theme: ThemeData(
-              brightness: Brightness.dark,
-              primaryColor: Color(0xFF121212),
-              accentColor: Colors.red,
-            ),
-            home: Scaffold(
-              appBar: AppBar(
-                      title: Text("HawkEye"),
-                      backgroundColor: Colors.redAccent,
-                      elevation: 50.0,
-                      actions: [
-                        IconButton(
-                          icon: Icon(Icons.logout),
-                          onPressed: () {
-                            auth.logout();
-                          },
-                        )
-                      ],
-                    )
-                 ,
-            ),
-    );
 }
