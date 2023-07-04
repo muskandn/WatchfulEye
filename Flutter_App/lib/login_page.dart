24f07c57e4c1b79363efe737a7d6f440c06ecf3e
SakshiGoyat <sakshigoyat29@gmail.com>
2023-07-04 19:17:05 +0530

    login page

---
+++ Flutter_App/lib/login_page.dart
@@ -0,0 +1,40 @@
+import 'sign_up_form.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isSigningIn = false; //bool flag to switch between sign in and sign up
+  void _showFormDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      child: AlertDialog(
+        //dialog window for authentication forms
+        titlePadding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+        contentPadding: EdgeInsets.symmetric(horizontal: 5),
+        title: Text(isSigningIn ? 'Sign In' : 'Sign Up'),
+        actionsPadding: EdgeInsets.only(right: 15, bottom: 5),
+        content: isSigningIn ? SignInForm() : SignUpForm(),
+        actions: <Widget>[
+          GestureDetector(
+            onTap: () {
+              //auth mode switcher
+              isSigningIn = !isSigningIn;
+              Navigator.of(context).pop();
+              _showFormDialog(context);
+            },
+            child: Text(
+              isSigningIn ? 'Sign Up instead' : 'Sign In instead',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
